2a1481aa2d5d03af80958d17cb4488bf4973b2e7
Julien Schneider <julien.schneider.1991@gmail.com>
2026-04-10 14:20:41 +0200

    fix

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,9 +2,9 @@ import type { StorybookConfig } from '@storybook/angular';
 
 const config: StorybookConfig = {
   stories: [
-    'libs/vre/ui/ui/src/**/*.stories.@(ts|mdx)',
-    'libs/vre/ui/progress-indicator/src/**/*.stories.@(ts|mdx)',
-    'libs/vre/pages/user-settings/user/src/**/*.stories.@(ts|mdx)',
+    '../libs/vre/ui/ui/src/**/*.stories.@(ts|mdx)',
+    '../libs/vre/ui/progress-indicator/src/**/*.stories.@(ts|mdx)',
+    '../libs/vre/pages/user-settings/user/src/**/*.stories.@(ts|mdx)',
   ],
   addons: [],
   framework: {
