52a88d34cee2b3db924737223c47c4af75f927ec
Marcin Procyk <marcin.procyk@dasch.swiss>
2026-04-22 12:15:50 +0200

    lint

--- libs/vre/resource-editor/resource-editor/src/lib/representations/still-image/osd-drawer.service.ts
+++ libs/vre/resource-editor/resource-editor/src/lib/representations/still-image/osd-drawer.service.ts
@@ -47,19 +47,16 @@ export class OsdDrawerService implements OnDestroy {
     this._subscribeToSelectedRegion();
     this._subscribeToCreatedRectangle();
 
-    this._osd.viewer.addHandler(
-      'canvas-click',
-      (event: OpenSeadragon.CanvasClickEvent) => {
-        event.preventDefaultAction = true;
-        const target = event.originalTarget as SVGElement;
-        const regionIri = target.getAttribute('data-region-iri');
-        if (regionIri === this._currentHighlightedRegion) {
-          this._regionService.setHighlightedRegionClicked(regionIri);
-        } else {
-          this._regionService.selectRegion(regionIri);
-        }
+    this._osd.viewer.addHandler('canvas-click', (event: OpenSeadragon.CanvasClickEvent) => {
+      event.preventDefaultAction = true;
+      const target = event.originalTarget as SVGElement;
+      const regionIri = target.getAttribute('data-region-iri');
+      if (regionIri === this._currentHighlightedRegion) {
+        this._regionService.setHighlightedRegionClicked(regionIri);
+      } else {
+        this._regionService.selectRegion(regionIri);
       }
-    );
+    });
   }
 
   update(resource: ReadResource): void {
