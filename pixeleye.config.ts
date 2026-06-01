fd32249299ff7cd5b4e87e0b8da03c8cf7a18ac0
Julien Schneider <julien.schneider.1991@gmail.com>
2026-06-01 16:43:48 +0200

    improve setup

--- pixeleye.config.ts
+++ pixeleye.config.ts
@@ -1,8 +1,4 @@
-import { defineConfig } from "pixeleye";
-
-export default defineConfig({
-  // Get this token from http://localhost:3000 after creating a project
+export default {
   token: process.env["PIXELEYE_TOKEN"] || "",
-  // Point at your local pixeleye instance
-  endpoint: "http://localhost:5000",
-});
+  endpoint: "http://localhost:5001",
+};
