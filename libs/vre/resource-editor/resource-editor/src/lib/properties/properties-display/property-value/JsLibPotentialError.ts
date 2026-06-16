1252f15ba1cde98a4c40ca1de83931fbb4cece73
domsteinbach <dominique.steinbach@unibas.ch>
2026-06-16 16:05:36 +0200

    feat: widen type

--- libs/vre/resource-editor/resource-editor/src/lib/properties/properties-display/property-value/JsLibPotentialError.ts
+++ libs/vre/resource-editor/resource-editor/src/lib/properties/properties-display/property-value/JsLibPotentialError.ts
@@ -1,7 +1,7 @@
-import { PropertyDefinition, ResourcePropertyDefinition } from '@dasch-swiss/dsp-js';
+import { PropertyDefinition, ResourcePropertyDefinitionWithAllLanguages } from '@dasch-swiss/dsp-js';
 
 export class JsLibPotentialError {
   static setAs(propertyDefinition: PropertyDefinition) {
-    return propertyDefinition as unknown as ResourcePropertyDefinition;
+    return propertyDefinition as unknown as ResourcePropertyDefinitionWithAllLanguages;
   }
 }
