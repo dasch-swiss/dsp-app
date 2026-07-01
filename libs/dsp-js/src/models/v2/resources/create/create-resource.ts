c204a85e22ad36dacf96d8cf2451cbb36e9da54e
domsteinbach <dominique.steinbach@unibas.ch>
2026-07-01 15:43:11 +0200

    chore: rm comment (confirmed)

--- libs/dsp-js/src/models/v2/resources/create/create-resource.ts
+++ libs/dsp-js/src/models/v2/resources/create/create-resource.ts
@@ -28,7 +28,6 @@ export class CreateResource {
   creationDate?: string = undefined;
 
   // Per-resource (data-side) authorship; serialized to knora-api:hasResourceAuthorship when set.
-  // TODO(verify-locally): confirm CreateResource serialization includes this @JsonProperty in the POST body.
   @JsonProperty(Constants.hasResourceAuthorship, [String], true)
   resourceAuthorship?: string[] = undefined;
 
